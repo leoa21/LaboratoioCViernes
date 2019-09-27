b27214e1d331b35500b768d28f0e3ba471d7e2f6
leoa21 <leo.21.alvarez@hotmail.com>
2019-09-27 08:47:50 -0500

    Practica8

--- main.c
+++ main.c
@@ -10,58 +10,32 @@ Horario: Viernes 7am-9am
 Matricula: 1986181
 */
 
-#define NUM 10
+
 int main(int argc, char *argv[]) {
 	
 	
 	
 	// *********Actividad 1*************
-
-		float cal=0,prom=0;
-		int i=0;
-		
-		
-		for(i=1;i<11;i++)
-		{
-			printf("Ingrese la calificacion %d: ",i);
-			scanf("%f",&cal);
-			
-			while(cal<0 || cal>10)
-			{
-			 printf("Dato incorrecto, ingrese la calificacion nuevamente: ");
-			 scanf("%f",&cal);
-			}
-			prom=prom+cal;
-			cal=0;
-		}	
-		
-		prom=prom/NUM;
-		if(prom>=7) 
-		{
-			printf("\nEl promedio del alumno fue: %.2f\nAPROBADO",prom);
-		}
-		else {
-			printf("\nEl promedio del alumno fue: %.2f\nREPOBRADO",prom);
-		}
+	
+	int numeroDeCoches[24]={ 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24}; 
+	int R=2;
+	printf("\t\tActividad 1 \nPrograma que muestra el total de coches en un hora en especifico\n\n");
+	numeroDeCoches[R]=57;
+	printf("En la hora %d hay un total de coches de %d\n",R,numeroDeCoches[R]);
+	
+	R=21;
+	numeroDeCoches[R]=57;
+	printf("En la hora %d hay un total de coches de %d\n",R,numeroDeCoches[R]);
+	
 
 	 	// *********Actividad 2*************
-
-	int b=0,e=0,r=1;
-	
-	printf("\n\nPrograma que calcula la potencia de un numero");
-	printf("\nIngrese el numero correspondiente a la de base: ");
-	scanf("%d",&b);
-	printf("Ingrese el numero correspondiente al exponente: ");
-	scanf("%d",&e);
-		
-	for(i=0;i<e;i++)
-	{
-		r=r*b;
-	}
-
-	printf("\nEl resultado de el numero %d elevado a la potencia %d es igual a: %d",b,e,r);
- 		
- 	 	
+ 
+ 	int A=8,B=4,Z=0;
+ 	printf("\n\t\tActividad 2 \nPrograma que determina el valor de la posicion que se encuetra en el resultado de la division de A y B\n");
+ 	printf("\nEl valor de A=%d\nEl valor de B=%d\n",A,B);
+ 	Z=A/B;
+ 	printf("El numero de coches en la posicion %d es %d",Z,numeroDeCoches[Z]);
+ 	
 	getch();
 	
 	return 0;
