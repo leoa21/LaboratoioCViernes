4fe7b5e52e9c68bf2e5066a17e290eef0da35f68
leoa21 <leo.21.alvarez@hotmail.com>
2019-11-01 10:00:57 -0600

    Practica13

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include<math.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<conio.h>
 
 /*
 Autor: Leonardo Damian Alvarez Mendoza
@@ -11,34 +11,33 @@ Horario: Viernes 7am-9am
 Matricula: 1986181
 */
 
-void primo(int a);
-int potencia(int z, int u);
-
+int sum(int *);
+void cont(char *);
+int x;
+void printfV(char lett, int val);
 int main(int argc, char *argv[]) {
 	
 	// *********Actividad 1*************
-	int x=0;
-	do 
-	{
-		printf("Ingrese un numero entero: ");
-		scanf("%d",&x);	
-	}while(x<0);
-	primo(x);
-	
-	
-	
+/*
+	char c[30];
+	printf("\t\tACTIVIDAD 1\n");
+	printf("Ingrese una palabra: ");
+	gets(c);
+	cont(c);
+*/	
 	
 	// *********Actividad 2*************
-printf("\n\n\t\tACTIVIDAD 2");
-int num,pot;
-
-printf("\nIngrese el numero: ");
-scanf("%d",&num);
-printf("Ingrese la potencia: ");
-scanf("%d",&pot);
-
-printf("El numero %d elevado a la potencia %d es igual: %d ",num,pot,potencia(num, pot));
-
+	int val[2];
+	
+	printf("\n");
+	printf("\t\tACTIVIDAD 2");
+	printf("\n");
+	printf("Ingrese el valor A: ");
+	scanf("%d",&val[0]);
+	printf("Ingrese el valor B: ");
+	scanf("%d",&val[1]);
+	
+	printf("La suma de %d + %d es igual a %d",val[0],val[1],sum(val));
 
 
 getch();
@@ -46,31 +45,46 @@ getch();
 return 0;
 }
 
-void primo(int a)
+int sum(int *val)
 {
-	int i,k=0;
+	int suma=0;
+	suma=*val;
+	suma=suma+*++val;
+	return suma;
+}
+/*
+void cont(char *c)
+{
+	int aa,ee,ii,oo,uu,j;
+	while(*c++)
+	{	
+			switch(tolower(*c))
+			{
+				case 'a': aa++;
+				break;
+				case 'e': ee++;
+				break;
+				case 'i': ii++;
+				break;
+				case 'o': oo++;
+				break;
+				case 'u': uu++;
+				break;
+			}
+	}	printf("\n\tVocales");
+	  	printfV('a',aa);
+		printfV('e',ee);
+		printfV('i',ii);
+		printfV('o',oo);
+		printfV('u',uu);	
+}
 		
-		for(i=1;i<a+1;i++)
-		if(a%i==0)
-		{
-			k=k+1;		
-		}
-
-	if(k==2)
-	{
-		printf("El numero %d es primo.",a);
-	}
-	else 
-	{
-		printf("El numero %d no es primo.",a);
-	}
-
-}
-
-int potencia(int z, int  u)
-{
-	int p=0;	
-	p=pow(z,u);
+		
 	
-	return p;
+
+void printfV(char lett, int val)
+{
+	if(val>0)
+	printf("\n%c: %d",lett,val);
 }
+*/
