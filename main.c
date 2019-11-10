e3f2860d90cc983e257cb3d36aad6ecf78f6712f
leoa21 <leo.21.alvarez@hotmail.com>
2019-11-09 21:42:00 -0600

    Practica14

--- main.c
+++ main.c
@@ -11,80 +11,136 @@ Horario: Viernes 7am-9am
 Matricula: 1986181
 */
 
-int sum(int *);
-void cont(char *);
-int x;
-void printfV(char lett, int val);
+
+
+#define SIZE_NAME 30
+#define SIZE 4
+struct pictures_info
+{
+	char name[SIZE_NAME];
+	int date_time;
+};
+
+struct cordinates
+{
+	int latitude;
+	int longitude;
+}places[SIZE];
+
+
 int main(int argc, char *argv[]) {
 	
 	// *********Actividad 1*************
-/*
-	char c[30];
-	printf("\t\tACTIVIDAD 1\n");
-	printf("Ingrese una palabra: ");
-	gets(c);
-	cont(c);
-*/	
 	
+	
+	 int i;
+	struct pictures_info pic[2];
+	struct pictures_info *ptr;
+	ptr=pic;
+	
+	for(i=0;i<2;i++)
+	{
+		fflush(stdin);
+		printf("Ingrese el nombre de la foto: ");
+		gets((ptr+i)->name);
+		printf("Ingrese la fecha: ");
+		scanf("%d",&(ptr+i)->date_time);
+		printf("\n");
+	}
+	
+	
+		
 	// *********Actividad 2*************
-	int val[2];
+	int a,j,aux;
+	struct cordinates *p;
+	int lon[4],lat[4];
+	
+	p=places;
+	
+	places[0].latitude=200;	
+	places[0].longitude=300;
+	places[1].latitude=400;
+	places[1].longitude=100;
+	places[2].latitude=100;
+	places[2].longitude=400;
+	places[3].latitude=300;
+	places[3].longitude=200;
+	
+	printf("\n\t\t\tActividad 2\n");	
+	
+	
+		printf("De que forma desea acceder a los datos?\n");
+		printf("\n1.Tal y como han sido agregados.\n2.Por latitud.\n3.Por longitud.\n\n");
+		scanf("%d",&a);	
+	
+		
+	
 	
 	printf("\n");
-	printf("\t\tACTIVIDAD 2");
-	printf("\n");
-	printf("Ingrese el valor A: ");
-	scanf("%d",&val[0]);
-	printf("Ingrese el valor B: ");
-	scanf("%d",&val[1]);
 	
-	printf("La suma de %d + %d es igual a %d",val[0],val[1],sum(val));
-
-
+	switch (a)
+	{
+		case 1: for (i=0;i<4;i++)
+			{
+				printf("Latidud: %d\n",places[i].latitude);
+				printf("Longitud:%d\n",places[i].longitude);
+			};
+			break;	
+			
+		case 2: for(i=0;i<4;i++)
+				{	
+				lat[i]=(p+i)->latitude;	
+				}
+				
+				for(i=1;i<4;i++)
+				{
+					for(j=0;j<3;j++)
+					{
+						if(lat[j]>lat[j+1])
+						{
+							aux=lat[j];
+							lat[j]=lat[j+1];
+							lat[j+1]=aux;
+						}
+					}
+				}
+				for(i=0;i<4;i++)
+				{
+					printf("Latitud: %d",lat[i]);
+					printf("\n");
+				}
+				break;
+				
+		case 3: aux=0;
+				for(i=0;i<4;i++)
+				{	
+					lon[i]=(p+i)->longitude;	
+				}
+	
+				for(i=1;i<4;i++)
+				{
+					for(j=0;j<3;j++)
+					{
+						if(lon[j]>lon[j+1])
+						{
+							aux=lon[j];
+							lon[j]=lon[j+1];
+							lon[j+1]=aux;
+						}
+					}
+				}
+				for(i=0;i<4;i++)
+				{
+				printf("Longitud: %d",lon[i]);
+				printf("\n");
+				}
+				break;
+		default: printf("INGRESO DATOS INCORRECTOS");		
+	}
+	
 getch();
 
 return 0;
 }
 
-int sum(int *val)
-{
-	int suma=0;
-	suma=*val;
-	suma=suma+*++val;
-	return suma;
-}
-/*
-void cont(char *c)
-{
-	int aa,ee,ii,oo,uu,j;
-	while(*c++)
-	{	
-			switch(tolower(*c))
-			{
-				case 'a': aa++;
-				break;
-				case 'e': ee++;
-				break;
-				case 'i': ii++;
-				break;
-				case 'o': oo++;
-				break;
-				case 'u': uu++;
-				break;
-			}
-	}	printf("\n\tVocales");
-	  	printfV('a',aa);
-		printfV('e',ee);
-		printfV('i',ii);
-		printfV('o',oo);
-		printfV('u',uu);	
-}
-		
-		
-	
 
-void printfV(char lett, int val)
-{
-	if(val>0)
-	printf("\n%c: %d",lett,val);
-}
-*/
